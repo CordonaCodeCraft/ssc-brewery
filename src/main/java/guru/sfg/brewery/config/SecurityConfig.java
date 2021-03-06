691034f58ec65740d4395ecdb743be153623112e
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-03-06 18:36:40 +0200

    Added new user and credentials.

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -38,6 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user")
                 .password("{noop}password")
                 .roles("USER");
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("scott")
+                .password("{noop}tiger")
+                .roles("CUSTOMER");
     }
 
 
