7dfef6533a51a96e436b13211332cb4115fc678f
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-03-07 08:52:42 +0200

    Decorated User and Authority with Lombok annotations.

--- src/main/java/guru/sfg/brewery/domain/security/Authority.java
+++ src/main/java/guru/sfg/brewery/domain/security/Authority.java
@@ -1,11 +1,20 @@
 package guru.sfg.brewery.domain.security;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
 /**
  * Created by jt on 6/21/20.
  */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 @Entity
 public class Authority {
 
